09267c80b17f74722e8eeab5d3cb50a892cbcf12
MAME SANDECK NIANG <63318185+NIANG95@users.noreply.github.com>
2023-01-31 12:10:01 +0000

    Update HelloWorlApp.java

--- src/main/java/org/msn/HelloWorlApp.java
+++ src/main/java/org/msn/HelloWorlApp.java
@@ -3,6 +3,6 @@ package org.msn;
 public class HelloWorlApp {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World !" );
     }
 }
